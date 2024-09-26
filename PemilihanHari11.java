3000ac79e1ac760a64e0dcc3134c8c4c2c6db7a3
haf5th <hafiflearning@gmail.com>
2024-09-26 09:38:22 +0700

    ifangka

--- PemilihanHari11.java
+++ PemilihanHari11.java
@@ -4,28 +4,39 @@ public class PemilihanHari11 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        String dayName, dayType;
+        System.out.println("Masukkan Hari (1-7)");
+        int hari = sc.nextInt();
 
-        System.out.println("Input day name: ");
-        dayName = sc.nextLine();
-        
-        switch (dayName.toLowerCase()) {
-            case "monday":
-            case "tuesday":
-            case "thursday":
-            case "friday":
-                dayType = "weekday";
-                break;
-            case "saturday":
-            case "sunday":
-                dayType = "weekend";
-                break;
-            default:
-                dayType = "invalid day name";
-            
+        if (hari >=1 && hari <=5){
+            System.out.println("Hari tersebut merupakan weekday");
+        }else if (hari == 6 || hari == 7){
+            System.out.println("Hari tersebut merupakan weekend");
+        }else {
+            System.out.println("Invalid number");
         }
+
+       // String dayName, dayType;
+
+        //System.out.println("Input day name: ");
+        //dayName = sc.nextLine();
         
-        System.out.println(dayName + " is a" + dayType);
+        //switch (dayName.toLowerCase()) {
+          //  case "monday":
+            //case "tuesday":
+            //case "thursday":
+            //case "friday":
+              //  dayType = "weekday";
+                //break;
+            //case "saturday":
+            //case "sunday":
+              //  dayType = "weekend";
+                //break;
+            //default:
+              //  dayType = "invalid day name";
+            
+        //}
+        
+        //System.out.println(dayName + " is a" + dayType);
 
 
     }
