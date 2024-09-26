d854863e1cecba224404606d132ca7a3d8dba39a
haf5th <hafiflearning@gmail.com>
2024-09-26 08:29:23 +0700

    ternary

--- PemilihanBilangan11.java
+++ PemilihanBilangan11.java
@@ -1,22 +1,26 @@
 import java.util.Scanner;
 
 public class PemilihanBilangan11 {
+
     public static void main(String[] args) {
        
         Scanner sc = new Scanner(System.in);
         
         System.out.println("Masukkan sebuah angka: ");
         int angka = sc.nextInt();
-        if (angka % 2 == 0)
-        {
-            System.out.println("Angka " + angka + " termasuk bilangan genap");
-        }
-        else 
-        {
-            System.out.println("Angka " + angka + " termasuk bilangan ganjil");
-        }
-
-        
+       // if (angka % 2 == 0)
+       // {
+       //     System.out.println("Angka " + angka + " termasuk bilangan genap");
+       // }
+       // else 
+       // {
+       //     System.out.println("Angka " + angka + " termasuk bilangan ganjil");
+       // }
+       
+       String hasil = angka % 2 == 0 ? "bilangan genap " : "bilangan ganjil ";
+       System.out.println(hasil);
     }
-    
 }
+
+
+    
